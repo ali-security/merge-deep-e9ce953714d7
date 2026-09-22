c08ef0c81cf4d8ee3ef1f53a8b0b3b98b98e5000
Seal <info@sealsecurity.io>
2026-09-22 10:53:52 +0000

    CVE-2021-26707

--- test.js
+++ test.js
@@ -134,4 +134,100 @@ describe('mergeDeep', function() {
     var actual = merge(fixture);
     assert.deepEqual(actual, fixture);
   });
+
+  it('should not clone invalid keys', function() {
+    var obj1 = { a: { b: 1 } };
+    var obj2 = JSON.parse('{ "a": { "c": 2 }, "constructor": { "keys": 42 } }');
+
+    var actual = merge({}, obj1, obj2);
+    assert.deepEqual(actual, { a: { b: 1, c: 2 } });
+    assert.notDeepEqual(actual.a, obj1.a);
+    assert.notDeepEqual(actual.a, obj2.a);
+    assert.notEqual(actual.keys, 42);
+    assert.notEqual(actual.constructor.keys, 42);
+  });
+
+  it('should allow being used for custom constructors', function() {
+    // The following setup code is a simple way to demonstrate multiple inheritance by merging the prototype of one class onto another
+    function Shape() {
+      this.type = '';
+    }
+
+    function Position(x, y) {
+      this.x = x || 0;
+      this.y = y || 0;
+    }
+
+    Position.prototype.stringify = function() {
+      return '(' + this.x + ', ' + this.y + ')';
+    };
+
+    function Moveable(x, y) {
+      Position.call(this, x, y);
+    }
+
+    // By making Moveable inherit from Position, allows us to test what happens when `constructor` is passed to `isValidKey`.
+    Moveable.prototype = Object.create(Position.prototype);
+    Moveable.prototype.constructor = Moveable;
+    Moveable.prototype = merge(Moveable.prototype, Position.prototype);
+
+    Moveable.prototype.move = function(x, y) {
+      this.x += x;
+      this.y += y;
+    };
+
+    Moveable.prototype.position = function() {
+      return this.stringify();
+    };
+
+    function Rectangle() {
+      Shape.call(this);
+      Moveable.call(this);
+      this.type = 'rectangle';
+    }
+
+    // Single inheritance using Object.create
+    Rectangle.prototype = Object.create(Shape.prototype);
+    Rectangle.prototype.constructor = Rectangle;
+
+    // This is the test to ensure that `merge-deep` can be used with prototypal inheritance
+    Rectangle.prototype = merge(Rectangle.prototype, Moveable.prototype);
+
+    var rectangle = new Rectangle();
+    assert.equal(rectangle.position(), '(0, 0)');
+    rectangle.move(10, 20);
+    assert.equal(rectangle.position(), '(10, 20)');
+  });
+
+  it('should not pollute Object.prototype through constructor.prototype', function() {
+    // Proof-of-concept payload from GHSL-2020-160 / CVE-2021-26707
+    var payload = JSON.parse('{ "constructor": { "prototype": { "isAdmin": true } } }');
+
+    var actual = merge({}, payload);
+
+    assert.equal(Object.prototype.isAdmin, undefined);
+    assert.equal({}.isAdmin, undefined);
+    assert.equal(({}).hasOwnProperty('isAdmin'), false);
+    assert.equal(actual.isAdmin, undefined);
+  });
+
+  it('should not pollute a prototype through the prototype key', function() {
+    function Victim() {}
+    var payload = JSON.parse('{ "prototype": { "isAdmin": true } }');
+
+    merge(Victim, payload);
+
+    assert.equal(Victim.prototype.isAdmin, undefined);
+    assert.equal(new Victim().isAdmin, undefined);
+  });
+
+  it('should not pollute Object.prototype through __proto__', function() {
+    var payload = JSON.parse('{ "__proto__": { "polluted": "yes" } }');
+
+    var actual = merge({}, payload);
+
+    assert.equal(Object.prototype.polluted, undefined);
+    assert.equal({}.polluted, undefined);
+    assert.equal(actual.polluted, undefined);
+  });
 });
